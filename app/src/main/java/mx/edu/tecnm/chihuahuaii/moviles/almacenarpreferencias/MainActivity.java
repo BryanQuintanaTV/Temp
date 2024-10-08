54bcfaf62b94987156c863e0119d687f7096ba14
Bryan Quintana <18308051220409@cbtis122.edu.mx>
2024-10-08 15:46:31 -0700

    Class 10-08-2024 Added new icons, Added the functionality oof the button "button_guardar"

--- app/src/main/java/mx/edu/tecnm/chihuahuaii/moviles/almacenarpreferencias/MainActivity.java
+++ app/src/main/java/mx/edu/tecnm/chihuahuaii/moviles/almacenarpreferencias/MainActivity.java
@@ -1,6 +1,15 @@
 package mx.edu.tecnm.chihuahuaii.moviles.almacenarpreferencias;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +17,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.jetbrains.annotations.TestOnly;
+
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText_nombre;
+    RadioGroup radioGroup;
+    RadioButton radioButton_planta, radioButton_animal, radioButton_figura;
+    CalendarView calendarView_fecha;
+    String selectedDate;
+    Button button_guardar, button_ver;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +39,56 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Vincular Objetos
+        editText_nombre = findViewById(R.id.editText_nombre);
+        radioGroup = findViewById(R.id.radioGroup);
+        radioButton_planta = findViewById(R.id.radioButton_planta);
+        radioButton_animal = findViewById(R.id.radioButton_animal);
+        radioButton_figura = findViewById(R.id.radioButton_figura);
+        calendarView_fecha = findViewById(R.id.calendarView_fecha);
+        button_guardar = findViewById(R.id.button_guardar);
+        button_ver = findViewById(R.id.button_ver);
+
+        // Código para bóton button_guardar
+        button_guardar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /**
+                *  El método getSharedPreferences() permite definir un nombre de archivo para guardar preferencias.
+                *  Este archivo se podrá usar cualquier actividad de la app.
+                * */
+
+                SharedPreferences sharedPreferences = getSharedPreferences("preferencias1", Context.MODE_PRIVATE);
+
+                // Declarar objeto para editar preferencias.
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+
+                // Recuperar y Guardar el nombre
+                String nombre = editText_nombre.getText().toString();
+                editor.putString("nombre", nombre);
+
+                // Recuperar y Guardar el tema
+                if (radioButton_planta.isChecked()) {
+                    editor.putString("tema", "Planta");
+                } else if (radioButton_animal.isChecked()) {
+                    editor.putString("tema", "Animal");
+                } else if (radioButton_figura.isChecked()) {
+                    editor.putString("tema", "Figura");
+                }
+
+                // Recuperar y Guardar la fecha
+                selectedDate = String.valueOf(calendarView_fecha.getDate());
+                editor.putString("fecha", selectedDate);
+
+                // Guardar cambios
+                editor.commit();
+
+                // Mensaje de confirmación
+                Toast.makeText(MainActivity.this, "Preferencias guardadas: " + selectedDate, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
     }
 }
